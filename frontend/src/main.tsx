c4bb525c7ad84503ae940e661ee964878561f7ae
Lillian Frost <lillianvfrost@gmail.com>
2025-06-04 10:59:41 -0400

    FE built by claude, BE too but I didnt tie it in yet (#2)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import RecipeApp from './api/claude_recipe'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RecipeApp />
   </StrictMode>,
 )
